b4bbaef0bc8ed4fea9a12790ca978bdb0604b8d2
Hzhong <hzhong@unsa.edu.pe>
2026-05-30 19:12:47 -0500

    Programa 2 creado

---
+++ examples/solucion2.c
@@ -0,0 +1,45 @@
+#include "../turtlec.h"
+
+void fractalTree(Turtle *turtle, float length, int depth);
+
+int main(void) {
+    TurtleApp *app = turtleAppCreate(800, 600, "Arbol Fractal");
+
+    if (app == NULL)
+        return 1;
+
+    Turtle *t = turtleAppGetTurtle(app);
+
+    turtlePenUp(t);
+    turtleGoTo(t, 200.0f, 550.0f);
+    turtlePenDown(t);
+
+    turtleSetColor(t, 255, 100, 0);
+    turtleSetSpeed(t, 5.0f);
+
+    fractalTree(t, 150.0f, 8);
+
+    turtleAppRun(app);
+    turtleAppDestroy(app);
+
+    return 0;
+}
+
+void fractalTree(Turtle *turtle, float length, int depth) {
+    if (depth == 0 || length < 5)
+        return;
+
+    turtleForward(turtle, length);
+
+    /* Rama izquierda */
+    turtleLeft(turtle, 30);
+    fractalTree(turtle, length * 0.8f, depth - 1);
+
+    /* Rama derecha */
+    turtleRight(turtle, 60);
+    fractalTree(turtle, length * 0.6f, depth - 1);
+
+    turtleLeft(turtle, 30);
+
+    turtleBackward(turtle, length);
+}
