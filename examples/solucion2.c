b947bb43724b65cf969092cb8b699c6e031d5895
Hzhong <hzhong@unsa.edu.pe>
2026-05-30 19:16:34 -0500

    Punto de inicio de (200,400) a (500,400)

--- examples/solucion2.c
+++ examples/solucion2.c
@@ -11,7 +11,7 @@ int main(void) {
     Turtle *t = turtleAppGetTurtle(app);
 
     turtlePenUp(t);
-    turtleGoTo(t, 200.0f, 550.0f);
+    turtleGoTo(t, 500.0f, 550.0f);
     turtlePenDown(t);
 
     turtleSetColor(t, 255, 100, 0);
